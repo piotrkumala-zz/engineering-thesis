448ff8604b97bcc865ef106ccbe09683ddf538ac
Piotr Kumala <piotrek.kumala@gmail.com>
2020-12-30 23:51:53 +0100

    move all chart rendering to separate coroutine scope, so it's thread safe when refreshing fragment,  add Utils.init(...) to stop app from whining about it, also add full support for dark theme to the chart

--- app/src/main/java/com/example/myapplication/ui/dashboard/DashboardFragment.kt
+++ app/src/main/java/com/example/myapplication/ui/dashboard/DashboardFragment.kt
@@ -1,9 +1,11 @@
 package com.example.myapplication.ui.dashboard
 
+import android.content.res.Configuration
 import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import androidx.core.content.ContextCompat
 import androidx.fragment.app.Fragment
 import com.example.myapplication.R
 import com.github.mikephil.charting.charts.LineChart
@@ -12,6 +14,7 @@ import com.github.mikephil.charting.components.XAxis
 import com.github.mikephil.charting.data.Entry
 import com.github.mikephil.charting.data.LineData
 import com.github.mikephil.charting.data.LineDataSet
+import com.github.mikephil.charting.utils.Utils
 import kotlinx.coroutines.Dispatchers
 import kotlinx.coroutines.GlobalScope
 import kotlinx.coroutines.launch
@@ -19,11 +22,19 @@ import kotlinx.coroutines.launch
 
 class DashboardFragment : Fragment() {
 
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        Utils.init(context)
+    }
+
     override fun onCreateView(
-        inflater: LayoutInflater,
-        container: ViewGroup?,
-        savedInstanceState: Bundle?
+            inflater: LayoutInflater,
+            container: ViewGroup?,
+            savedInstanceState: Bundle?
     ): View? {
+        val root = inflater.inflate(R.layout.fragment_dashboard, container, false)
+        val lineChart: LineChart = root.findViewById(R.id.lineChart)
+
         //Part1
         val entries = ArrayList<Entry>()
 
@@ -32,65 +43,66 @@ class DashboardFragment : Fragment() {
         entries.add(Entry(2f, 2f))
         entries.add(Entry(3f, 7f))
 
-        ArrayList<Entry>()
-        entries.add(Entry(3.5f, 0f))
-        entries.add(Entry(4f, 20f))
-        entries.add(Entry(5f, 16f))
+        val entries1 = ArrayList<Entry>()
+        entries1.add(Entry(3.5f, 0f))
+        entries1.add(Entry(4f, 20f))
+        entries1.add(Entry(5f, 16f))
 
-        var newEntries = ArrayList<Entry>()
+        var newEntries: java.util.ArrayList<Entry>
         GlobalScope.launch(Dispatchers.IO) {
             newEntries = prepareEntires(entries)
-        }
 
-//Part3
+            val vl = LineDataSet(newEntries, "My Type")
+            val v2 = LineDataSet(entries1, "My Type")
 
-        val vl = LineDataSet(newEntries, "My Type")
-//        val v2 = LineDataSet(entries1, "My Type")
-
-        val legendEntries = ArrayList<LegendEntry>()
-        val legendEntry = LegendEntry()
-        legendEntry.label = "My Type"
-        legendEntry.formColor = vl.color
-        legendEntries.add(legendEntry)
+            val legendEntries = ArrayList<LegendEntry>()
+            val legendEntry = LegendEntry()
+            legendEntry.label = "My Type"
+            legendEntry.formColor = vl.color
+            legendEntries.add(legendEntry)
 
 
+            vl.setDrawValues(true)
+            vl.setDrawFilled(true)
+            vl.lineWidth = 2f
 
-//Part4
-        vl.setDrawValues(true)
-        vl.setDrawFilled(true)
-        vl.lineWidth = 2f
+            v2.setDrawValues(true)
+            v2.setDrawFilled(true)
+            v2.lineWidth = 2f
 
-        val root = inflater.inflate(R.layout.fragment_dashboard, container, false)
-        val lineChart: LineChart = root.findViewById(R.id.lineChart)
 
-        lineChart.xAxis.labelRotationAngle = 0f
+            lineChart.xAxis.labelRotationAngle = 0f
 
-//Part6
-        lineChart.data = LineData(vl)
-//        lineChart.data.addDataSet(v2)
+            lineChart.data = LineData(vl)
+            lineChart.data.addDataSet(v2)
 
-        lineChart.legend.setCustom(legendEntries)
+            lineChart.legend.setCustom(legendEntries)
 
-//Part7
-        lineChart.axisRight.isEnabled = false
-        lineChart.xAxis.position = XAxis.XAxisPosition.BOTTOM
+            lineChart.axisRight.isEnabled = false
+            lineChart.xAxis.position = XAxis.XAxisPosition.BOTTOM
 
-//Part8
-        lineChart.setTouchEnabled(true)
-        lineChart.setPinchZoom(true)
+            lineChart.setTouchEnabled(true)
+            lineChart.setPinchZoom(true)
 
-        lineChart.data
+            lineChart.data
 
-//Part9
-        lineChart.description.text = "Days"
-        lineChart.setNoDataText("No forex yet!")
+            lineChart.description.isEnabled = false
 
 //Part10
-
-
-//Part11
+            if (context?.resources?.configuration?.uiMode?.and(Configuration.UI_MODE_NIGHT_MASK) == Configuration.UI_MODE_NIGHT_YES) {
+                lineChart.xAxis.axisLineColor = ContextCompat.getColor(requireContext(), R.color.white)
+                lineChart.xAxis.gridColor = ContextCompat.getColor(requireContext(), R.color.white)
+                lineChart.xAxis.textColor = ContextCompat.getColor(requireContext(), R.color.white)
+                lineChart.axisLeft.axisLineColor = ContextCompat.getColor(requireContext(), R.color.white)
+                lineChart.axisLeft.gridColor = ContextCompat.getColor(requireContext(), R.color.white)
+                lineChart.axisLeft.textColor = ContextCompat.getColor(requireContext(), R.color.white)
+                lineChart.legend.textColor = ContextCompat.getColor(requireContext(), R.color.white)
+                lineChart.data.dataSets.forEach { set ->
+                    set.valueTextColor = ContextCompat.getColor(requireContext(), R.color.white)
+                }
+            }
+        }
         return root
-
     }
 
     private fun prepareEntires(entries: ArrayList<Entry>): ArrayList<Entry> {
